86d8d599ef87e17dc30443d263777320f797c246
hatanotakahiro <3a07katouken@gmail.com>
2022-04-27 21:48:45 +0900

    ログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:authentication_keys => [:name]
+
+  # No use email
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 
   has_many :equipments
   has_many :rentals
