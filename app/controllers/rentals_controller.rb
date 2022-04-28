8a7991873d951756ad61ea9ec53a3a70e3209a70
hatanotakahiro <3a07katouken@gmail.com>
2022-04-28 20:39:32 +0900

    index修正

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,14 @@
 class RentalsController < ApplicationController
-  before_action :authenticate_user!, except: [ :index]
+  before_action :authenticate_user!, except: [ :index, :search]
   def index
+      rentals = Rental.includes(:user).order("created_at DESC")
+      @rentals = rentals.page(params[:page]).per(8)
+      @new_rental = Rental.new
+  end
+
+  def search
     @rental = Rental.includes(:user).order("created_at DESC")
+    @rentals = Rental.where('movie_title LIKE ?', "%#{movie_params[:tag_list]}%").order("created_at DESC")
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root to: 'rentals#index'
+
+  get 'rentals/search', to: 'rentals#search'
+  post 'rentals/search', to: 'rentals#search'
 end
